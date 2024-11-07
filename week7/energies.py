fc948abe15fb3842710f1cf4bea13f3fc8435017
Alexander Holman <alexandere@holman.email>
2024-11-07 15:57:33 +0000

    readd relaxation

--- week7/energies.py
+++ week7/energies.py
@@ -1,7 +1,6 @@
 from copy import deepcopy
-
 from ase.build import stack, make_supercell
-from ase.optimize import FIRE
+from ase.optimize import FIRE, BFGS
 from mace.calculators import mace_mp
 from ase.io import write
 from numpy.ma.extras import average
@@ -12,7 +11,7 @@ from classes.POSCAR import POSCAR
 
 macemp = mace_mp(model="large", dispersion=True, default_dtype="float64")
 
-atoms_Si, atoms_Ge = POSCAR.get_bulk_structure('Si').atoms, POSCAR.get_bulk_structure('Ge').atoms
+atoms_Si, atoms_Ge = POSCAR.get_relaxed_bulk_structure('Si').atoms, POSCAR.get_relaxed_bulk_structure('Ge').atoms
 atoms_Si.calc = atoms_Ge.calc = macemp
 
 atoms_SiGe_8_poscar = POSCAR.get_bulk_structure('Si')
@@ -26,8 +25,8 @@ a_SiGe = average((a_Si, a_Ge))
 atoms_SiGe_8 = atoms_SiGe_8_poscar.atoms
 atoms_SiGe_8.cell = [[a_SiGe, 0, 0], [0, a_SiGe, 0], [0, 0, a_SiGe]]
 atoms_SiGe_8.calc = atoms_Si.calc
-# atoms_Si_Ge_8_optimizer = FIRE(atoms_SiGe_8)
-# atoms_Si_Ge_8_optimizer.run(fmax=0.001)
+atoms_Si_Ge_8_optimizer = FIRE(atoms_SiGe_8)
+atoms_Si_Ge_8_optimizer.run(fmax=0.001)
 
 E_SiGe_8_a_SiGe = atoms_SiGe_8.get_potential_energy()
 E_SiGe_8_a_SiGe_per_atom = E_SiGe_8_a_SiGe / len(atoms_SiGe_8)
@@ -44,8 +43,8 @@ E_Ge_a_Ge_per_atom = E_Ge_a_Ge / len(atoms_Ge)
 atoms_Si_strained = deepcopy(atoms_Si)
 atoms_Si_strained.cell = [[a_SiGe, 0, 0], [0, a_SiGe, 0], [0, 0, a_SiGe]]
 atoms_Si_strained.calc = atoms_Si.calc
-# atoms_Si_strained_optimizer = FIRE(atoms_Si_strained)
-# atoms_Si_strained_optimizer.run(fmax=0.001)
+atoms_Si_strained_optimizer = FIRE(atoms_Si_strained)
+atoms_Si_strained_optimizer.run(fmax=0.001)
 
 write('structures/energies-unrelaxed/Si_a_SiGe.vasp', atoms_Si_strained, 'vasp')
 
@@ -57,8 +56,8 @@ E_Si_strain = E_Si_a_SiGe - E_Si_a_Si
 atoms_Ge_strained = deepcopy(atoms_Ge)
 atoms_Ge_strained.cell = [[a_SiGe, 0, 0], [0, a_SiGe, 0], [0, 0, a_SiGe]]
 atoms_Ge_strained.calc = atoms_Ge.calc
-# atoms_Ge_strained_optimizer = FIRE(atoms_Ge_strained)
-# atoms_Ge_strained_optimizer.run(fmax=0.001)
+atoms_Ge_strained_optimizer = FIRE(atoms_Ge_strained)
+atoms_Ge_strained_optimizer.run(fmax=0.001)
 
 write('structures/energies-unrelaxed/Ge_a_SiGe.vasp', atoms_Ge_strained, 'vasp')
 
@@ -69,8 +68,8 @@ E_Ge_strain = E_Ge_a_SiGe - E_Ge_a_Ge
 
 atoms_SiGe = stack(atoms_Si, atoms_Ge)
 atoms_SiGe.calc = atoms_Si.calc
-# atoms_SiGe_optimizer = FIRE(atoms_SiGe)
-# atoms_SiGe_optimizer.run(fmax=0.001)
+atoms_SiGe_optimizer = FIRE(atoms_SiGe)
+atoms_SiGe_optimizer.run(fmax=0.001)
 
 write('structures/energies-unrelaxed/SiGe_a_SiGe.vasp', atoms_SiGe, 'vasp')
 
