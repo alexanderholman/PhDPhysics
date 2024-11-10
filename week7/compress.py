cabad4a517584ee1e1d72807f6f951ae09918c58
Alexander Holman <alexandere@holman.email>
2024-11-10 21:14:49 +0000

    small updates to check eq

--- week7/compress.py
+++ week7/compress.py
@@ -1,20 +1,21 @@
 from copy import deepcopy
-
 from ase.build import bulk, stack
-from ase.visualize import view
 from ase.optimize import FIRE
 from mace.calculators import mace_mp
 
-si_bulk = bulk("Si", crystalstructure='diamond', a=5.4310, cubic=True)
-ge_bulk = bulk("Ge", crystalstructure='diamond', a=5.6578, cubic=True)
+SiLATCONST = 5.4310
+GeLATCONST = 5.6578
+
+si_bulk = bulk("Si", crystalstructure='diamond', a=SiLATCONST, cubic=True)
+ge_bulk = bulk("Ge", crystalstructure='diamond', a=GeLATCONST, cubic=True)
 ge_bulk.calc = mace_mp(model="large", dispersion=True, default_dtype="float64")
 
 stack_bulk = stack(si_bulk, ge_bulk, axis=0)
 
-lattice_constant = stack_bulk.cell[1][1]
+SiGeLATCONST = stack_bulk.cell[1][1]
 
 ge_compression = deepcopy(ge_bulk)
-ge_compression.set_cell([[lattice_constant, 0, 0], [0, lattice_constant, 0], [0, 0, lattice_constant]], scale_atoms=True)
+ge_compression.set_cell([[SiGeLATCONST, 0, 0], [0, SiGeLATCONST, 0], [0, 0, SiGeLATCONST]], scale_atoms=True)
 
 ge_bulk_optimizer = FIRE(ge_bulk)
 ge_bulk_optimizer.run(fmax=0.001)
@@ -24,7 +25,22 @@ ge_compression_optimizer.run(fmax=0.001)
 
 strain_energy = compression_energy = ge_compression.get_potential_energy() - ge_bulk.get_potential_energy()
 
-print(f"Strain/compression energy: {strain_energy}")
+print(f"Ge Strain/compression energy ({GeLATCONST} to {SiGeLATCONST}): {strain_energy}, per atom: {strain_energy / len(ge_compression)}")
 
-view(ge_bulk)
-view(ge_compression)
+# SI equivalent
+si_bulk.calc = ge_bulk.calc
+
+EqSiGeLATCONST = SiGeLATCONST / GeLATCONST * SiLATCONST
+
+si_compression = deepcopy(si_bulk)
+si_compression.set_cell([[EqSiGeLATCONST, 0, 0], [0, EqSiGeLATCONST, 0], [0, 0, EqSiGeLATCONST]], scale_atoms=True)
+
+si_bulk_optimizer = FIRE(si_bulk)
+si_bulk_optimizer.run(fmax=0.001)
+
+si_compression_optimizer = FIRE(si_compression)
+si_compression_optimizer.run(fmax=0.001)
+
+strain_energy = compression_energy = si_compression.get_potential_energy() - si_bulk.get_potential_energy()
+
+print(f"Si Strain/compression energy ({SiLATCONST} to {EqSiGeLATCONST}): {strain_energy}, per atom: {strain_energy / len(si_compression)}")
--- week7/tension.py
+++ week7/tension.py
@@ -1,20 +1,21 @@
 from copy import deepcopy
-
 from ase.build import bulk, stack
-from ase.visualize import view
 from ase.optimize import FIRE
 from mace.calculators import mace_mp
 
-si_bulk = bulk("Si", crystalstructure='diamond', a=5.4310, cubic=True)
-ge_bulk = bulk("Ge", crystalstructure='diamond', a=5.6578, cubic=True)
+SiLATCONST = 5.4310
+GeLATCONST = 5.6578
+
+si_bulk = bulk("Si", crystalstructure='diamond', a=SiLATCONST, cubic=True)
+ge_bulk = bulk("Ge", crystalstructure='diamond', a=GeLATCONST, cubic=True)
 si_bulk.calc = mace_mp(model="large", dispersion=True, default_dtype="float64")
 
 stack_bulk = stack(si_bulk, ge_bulk, axis=0)
 
-lattice_constant = stack_bulk.cell[1][1]
+SiGeLATCONST = stack_bulk.cell[1][1]
 
 si_tension = deepcopy(si_bulk)
-si_tension.set_cell([[lattice_constant, 0, 0], [0, lattice_constant, 0], [0, 0, lattice_constant]], scale_atoms=True)
+si_tension.set_cell([[SiGeLATCONST, 0, 0], [0, SiGeLATCONST, 0], [0, 0, SiGeLATCONST]], scale_atoms=True)
 
 si_bulk_optimizer = FIRE(si_bulk)
 si_bulk_optimizer.run(fmax=0.001)
@@ -24,7 +25,23 @@ si_tension_optimizer.run(fmax=0.001)
 
 strain_energy = tension_energy = si_tension.get_potential_energy() - si_bulk.get_potential_energy()
 
-print(f"Strain/tension energy: {strain_energy}")
+print(f"Si Strain/tension energy ({SiLATCONST} to {SiGeLATCONST}): {strain_energy}, per atom: {strain_energy / len(si_tension)}")
 
-view(si_bulk)
-view(si_tension)
+# GE equivalent
+ge_bulk.calc = si_bulk.calc
+
+EqGeLATCONST = SiGeLATCONST / SiLATCONST * GeLATCONST
+
+ge_tension = deepcopy(ge_bulk)
+
+ge_tension.set_cell([[EqGeLATCONST, 0, 0], [0, EqGeLATCONST, 0], [0, 0, EqGeLATCONST]], scale_atoms=True)
+
+ge_bulk_optimizer = FIRE(ge_bulk)
+ge_bulk_optimizer.run(fmax=0.001)
+
+ge_tension_optimizer = FIRE(ge_tension)
+ge_tension_optimizer.run(fmax=0.001)
+
+strain_energy = tension_energy = ge_tension.get_potential_energy() - ge_bulk.get_potential_energy()
+
+print(f"Ge Strain/tension energy ({GeLATCONST} to {EqGeLATCONST}): {strain_energy}, per atom: {strain_energy / len(ge_tension)}")
